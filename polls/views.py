a7eb83c32a434d7be314fb02b0e3917d3c7e19fd
bryan_menendez <bryan.menendez@protonmail.com>
2021-04-27 22:37:31 -0300

    class based views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,28 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def details(request, question_id):
-    # try:
-    #     q = Question.objects.get(id=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'q_list'
 
-    q = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': q})
+    def get_queryset(self):
+        """return the last fivve publications"""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    q = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': q})
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -41,3 +46,19 @@ def index(request):
     q_list = Question.objects.order_by('-pub_date')[:5]
     context = {'q_list': q_list}
     return render(request,'polls/index.html', context)
+
+
+def details(request, question_id):
+    # try:
+    #     q = Question.objects.get(id=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question': q})
+
+
+def results(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': q})
+
